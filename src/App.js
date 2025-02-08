8e2598e53fecf228b870c33e2eb4306aea55a0a2
Birry <bridger.woller@gmail.com>
2025-02-07 22:41:44 -0600

    Testing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello, World!</h1>
+        <h1>Wow this is a website I am hosting all by myself!</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
